66019240bbececb897e06fd42210dd34eb40600d
syzwina <nor.syazwina.ahmad.fazrul@gmail.com>
2024-11-10 10:38:38 +0000

    api set up

--- app/admin.py
+++ app/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import UploadedImage
 
 # Register your models here.
+
+class UploadedImageAdmin(admin.ModelAdmin):
+    list_display = ('image', 'uploaded_at')
+
+# Register your models here.
+
+admin.site.register(UploadedImage, UploadedImageAdmin)
